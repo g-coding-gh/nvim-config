3ff63bd57bafde3dcba330bbdf06a91b17f0c985
g-coding-gh <g.coding.email@gmail.com>
2024-01-19 23:13:24 -0500

    added a few more telescope kb and tried to add more plugins

---
+++ lua/plugins/buffer-line.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/bufferline.nvim',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup{
+      options = {
+        hover =  {
+          enabled = true,
+          delay = 150,
+          reveal = {'close'}
+        }
+      }
+    }
+  end
+}
